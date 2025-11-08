aab957df4dfc96baa78d069952b4f950f66d3469
bhanuteja1403 <bunnykodurupaka@gmail.com>
2025-11-08 21:33:58 +0530

    Hyper Parameter Tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,6 @@ import os
 from src.utils import save_object
 
 @dataclass
-
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 
@@ -17,22 +18,31 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-
-def evaluate_models(X_train,y_train,X_test,y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            parameters = param[list(models.keys())[i]]
 
+            # Perform GridSearchCV for hyperparameter tuning
+            gs = GridSearchCV(model, parameters, cv=3, n_jobs=-1, verbose=1)
+            gs.fit(X_train, y_train)
+
+            # Update model with best parameters
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
+            # Predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
+            # R² Scores
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
+            # Store model performance
             report[list(models.keys())[i]] = test_model_score
 
         return report
